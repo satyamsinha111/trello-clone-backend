c6d69e05794b08d13539a3a094d839e7f8213d3d
satyamsinha111 <satyamsinha8158@gmail.com>
2021-12-05 17:49:55 +0530

    restructured the code base

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middlewares");
-
+const { connection } = require("../db");
 router.use(auth.jwt_decode);
 
 router.post("/project", (req, res) => {
   console.log(req.user);
+  connection.query(
+    `SELECT * FROM Users Where Email = '${req.user.email}'`,
+    (err, rows, field) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(rows[0].UserID);
+        const userId = rows[0].UserID;
+        let values = [req.body.proj_name, req.body.proj_desc];
+        connection.query(
+          "INSERT INTO Projects (ProjectName,ProjectDesc) VALUES(?)",
+          [values],
+          (err, rows, fields) => {
+            if (err) {
+              throw err;
+            } else {
+              //   console.log(rows, fields);
+              let values = [rows.insertId, undefined, userId];
+              connection.query(
+                "INSERT INTO ProjectTaskUserMapping (ProjectID,TaskID,UserID) VALUES (?)",
+                [values],
+                (err, rows, fields) => {
+                  if (err) {
+                    throw err;
+                  } else {
+                    console.log(rows);
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    }
+  );
   res.send("Hello");
 });
 
